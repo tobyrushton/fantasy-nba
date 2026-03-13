5bf56541c6e4bf9369f523168e3d24d086cb1e04
tobyrushton <tobyjrushton@icloud.com>
2026-03-13 00:29:59 +0000

    scraper: add GetPlayers()

---
+++ pkg/scraper/players_test.go
@@ -0,0 +1,39 @@
+package scraper_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/tobyrushton/fantasy-nba/pkg/fakes"
+	"github.com/tobyrushton/fantasy-nba/pkg/models"
+	"github.com/tobyrushton/fantasy-nba/pkg/scraper"
+)
+
+func TestScraper_GetPlayers(t *testing.T) {
+	c := &fakes.FakeClient{}
+	s := scraper.New(c)
+
+	rc := getReadCloser(t, "./testdata/nuggets_profile.html")
+	c.DoReturns(&http.Response{
+		StatusCode: http.StatusOK,
+		Body:       rc,
+	}, nil)
+
+	teams := []models.Team{
+		{
+			NBAID:        "1610612743",
+			Name:         "Denver Nuggets",
+			Abbreviation: "DEN",
+		},
+	}
+
+	players, err := s.GetPlayers(teams)
+	if err != nil {
+		t.Fatalf("failed to get players: %v", err)
+	}
+
+	// The Nuggets had 18 players on their roster at the time of writing this test.
+	if len(players) != 18 {
+		t.Fatalf("expected 18 players, got %d", len(players))
+	}
+}
