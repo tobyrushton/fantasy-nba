c9b297b9ccb9ec4df47253672317f25391cb32c0
tobyrushton <tobyjrushton@icloud.com>
2026-03-13 18:11:50 +0000

    migrate: add basic db migration command

---
+++ cmd/migrate/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tobyrushton/fantasy-nba/pkg/config"
+	"github.com/tobyrushton/fantasy-nba/pkg/db"
+	"github.com/tobyrushton/fantasy-nba/pkg/db/models"
+)
+
+func main() {
+	cfg := config.MustLoadConfig()
+
+	db, err := db.NewDb(fmt.Sprintf("postgres://admin:%s@localhost:5432/postgres?sslmode=disable", cfg.DB_PASSWORD))
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	ctx := context.Background()
+
+	_, err = db.NewCreateTable().
+		Model((*models.Team)(nil)).
+		IfNotExists().
+		Exec(ctx)
+	if err != nil {
+		log.Fatalf("failed to create teams table: %v", err)
+	}
+
+	_, err = db.NewCreateTable().
+		Model((*models.Player)(nil)).
+		IfNotExists().
+		Exec(ctx)
+	if err != nil {
+		log.Fatalf("failed to create players table: %v", err)
+	}
+
+	_, err = db.NewCreateTable().
+		Model((*models.Game)(nil)).
+		IfNotExists().
+		Exec(ctx)
+	if err != nil {
+		log.Fatalf("failed to create games table: %v", err)
+	}
+
+	_, err = db.NewCreateTable().
+		Model((*models.PlayerGameStats)(nil)).
+		IfNotExists().
+		Exec(ctx)
+	if err != nil {
+		log.Fatalf("failed to create player_game_stats table: %v", err)
+	}
+
+	log.Println("Database migration completed successfully.")
+}
