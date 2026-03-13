6cd92bbec1fc2646e1b27e33da316a5be8b4ac35
tobyrushton <tobyjrushton@icloud.com>
2026-03-13 11:52:44 +0000

    scraper: add additional team scraping tests

--- pkg/scraper/players_test.go
+++ pkg/scraper/players_test.go
@@ -13,7 +13,13 @@ func TestScraper_GetPlayers(t *testing.T) {
 	s := New(c)
 
 	rc := getReadCloser(t, "./testdata/nuggets_profile.html")
-	c.DoReturns(&http.Response{
+	c.DoReturnsOnCall(0, &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       rc,
+	}, nil)
+
+	rc = getReadCloser(t, "./testdata/warriors_profile.html")
+	c.DoReturnsOnCall(1, &http.Response{
 		StatusCode: http.StatusOK,
 		Body:       rc,
 	}, nil)
@@ -24,9 +30,42 @@ func TestScraper_GetPlayers(t *testing.T) {
 			Name:         "Denver Nuggets",
 			Abbreviation: "DEN",
 		},
+		{
+			NBAID:        "1610612744",
+			Name:         "Golden State Warriors",
+			Abbreviation: "GSW",
+		},
 	}
 
 	players, err := s.GetPlayers(teams)
+	if err != nil {
+		t.Fatalf("failed to get players: %v", err)
+	}
+
+	// The Nuggets had 18 players on their roster at the time of writing this test.
+	// The Warriors have 17 so in total 35 players should be found.
+	if len(players) != 35 {
+		t.Fatalf("expected 35 players, got %d", len(players))
+	}
+}
+
+func TestScraper_getPlayersForTeam(t *testing.T) {
+	team := models.Team{
+		NBAID:        "1610612743",
+		Name:         "Denver Nuggets",
+		Abbreviation: "DEN",
+	}
+
+	c := &fakes.FakeClient{}
+	s := New(c)
+
+	rc := getReadCloser(t, "./testdata/nuggets_profile.html")
+	c.DoReturnsOnCall(0, &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       rc,
+	}, nil)
+
+	players, err := s.getPlayersForTeam(team)
 	if err != nil {
 		t.Fatalf("failed to get players: %v", err)
 	}
