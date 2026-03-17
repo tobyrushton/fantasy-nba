fb84f981ac75d9c1a04c3eb664ee6e1b3505ab3a
tobyrushton <tobyjrushton@icloud.com>
2026-03-17 00:16:52 +0000

    cors: allow frontend url

--- main.go
+++ main.go
@@ -32,7 +32,13 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:4321", "http://127.0.0.1:4321", "http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowOrigins: []string{
+			"http://localhost:4321",
+			"http://127.0.0.1:4321",
+			"http://localhost:3000",
+			"http://127.0.0.1:3000",
+			"https://fantasy-nba-1.onrender.com",
+		},
 		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 	}))
